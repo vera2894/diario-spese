a143b4589a901b5786185799a7b2af2d3df3b1ce
vera2894 <abrami.veronica@gmail.com>
2026-03-12 13:47:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diario-spese-v2';
+const CACHE = 'diario-spese-v3';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
