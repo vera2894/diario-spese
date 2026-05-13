606bffa2d2195ef00235084c5b232526d5444f2f
vera2894 <abrami.veronica@gmail.com>
2026-05-13 16:16:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diario-spese-v9';
+const CACHE = 'diario-spese-v10';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
